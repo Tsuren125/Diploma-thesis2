020fa24fc87602df6b8ef4b9cb625c51a31c9f6f
Tsuren125 <gom6oev@gmail.com>
2025-10-19 21:32:11 +0900

    Update cart_page.py

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -2,16 +2,19 @@ from selenium.webdriver.common.by import By
 from .base_page import BasePage
 
 
-
-
 class CartPage(BasePage):
-CHECKOUT_BTN = (By.CSS_SELECTOR, 'a.checkout, button.checkout')
-CART_ITEMS = (By.CSS_SELECTOR, '.cart-item, .basket-list__item')
+    CHECKOUT_BTN = (
+        By.CSS_SELECTOR,
+        'a.checkout, button.checkout'
+    )
+    CART_ITEMS = (
+        By.CSS_SELECTOR,
+        '.cart-item, .basket-list__item'
+    )
 
+    def open_cart(self):
+        self.open('/cart')
 
-def open_cart(self):
-self.open('/cart')
+    def get_items(self):
+        return self.finds(self.CART_ITEMS)
 
-
-def get_items(self):
-return self.finds(self.CART_ITEMS)
