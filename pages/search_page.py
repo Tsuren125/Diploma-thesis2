d97aa504c75a1f75d1d9ffb47b87bb513c0ed45c
Tsuren125 <gom6oev@gmail.com>
2025-10-19 21:31:22 +0900

    Update search_page.py

--- pages/search_page.py
+++ pages/search_page.py
@@ -2,19 +2,21 @@ from selenium.webdriver.common.by import By
 from .base_page import BasePage
 
 
-
-
 class SearchPage(BasePage):
-PRODUCT_CARD = (By.CSS_SELECTOR, '.product-card, .product, .catalog-item')
-PRODUCT_TITLE = (By.CSS_SELECTOR, '.product-title, .product-name, .catalog-item__title')
+    PRODUCT_CARD = (
+        By.CSS_SELECTOR,
+        '.product-card, .product, .catalog-item'
+    )
+    PRODUCT_TITLE = (
+        By.CSS_SELECTOR,
+        '.product-title, .product-name, .catalog-item__title'
+    )
 
+    def get_results(self):
+        return self.finds(self.PRODUCT_CARD)
 
-def get_results(self):
-return self.finds(self.PRODUCT_CARD)
-
-
-def open_first_result(self):
-cards = self.get_results()
-if not cards:
-raise AssertionError('Search returned no results')
-cards[0].click()
+    def open_first_result(self):
+        cards = self.get_results()
+        if not cards:
+            raise AssertionError('Search returned no results')
+        cards[0].click()
