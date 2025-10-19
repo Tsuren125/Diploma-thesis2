74f91c298fa1e935e7d6d896481c9a61f873a94f
Tsuren125 <gom6oev@gmail.com>
2025-10-19 20:59:08 +0900

    Create test_data.py

---
+++ data/test_data.py
@@ -0,0 +1,7 @@
+TEST_USER = {
+'email': os.getenv('TEST_USER_EMAIL', 'your_test_user@example.com'),
+'password': os.getenv('TEST_USER_PASSWORD', 'ChangeMe123')
+}
+
+
+SEARCH_QUERY = 'властелин колец' # пример поискового запроса (можно заменить)
