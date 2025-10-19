f4d6a60c81bf4105f4746eadb9b8e672e73acec4
Tsuren125 <gom6oev@gmail.com>
2025-10-19 21:33:08 +0900

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -8,43 +8,36 @@ from pages.home_page import HomePage
 from pages.login_page import LoginPage
 
 
-
-
 @pytest.fixture(scope='session')
 def base_url():
-return BASE_URL
-
-
+    return BASE_URL
 
 
 @pytest.fixture(scope='session')
 def api_client():
-return ApiClient()
-
-
+    return ApiClient()
 
 
 @pytest.fixture
 def driver():
-options = webdriver.ChromeOptions()
-if HEADLESS:
-options.add_argument('--headless=new')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-yield driver
-driver.quit()
-
-
+    options = webdriver.ChromeOptions()
+    if HEADLESS:
+        options.add_argument('--headless=new')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()),
+        options=options
+    )
+    yield driver
+    driver.quit()
 
 
 @pytest.fixture
 def home_page(driver, base_url):
-return HomePage(driver, base_url)
-
-
+    return HomePage(driver, base_url)
 
 
 @pytest.fixture
 def login_page(driver, base_url):
-return LoginPage(driver, base_url)
+    return LoginPage(driver, base_url)
