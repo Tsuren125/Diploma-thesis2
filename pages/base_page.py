7626e3076d8594ccb49683d58b9cc7409755333d
Tsuren125 <gom6oev@gmail.com>
2025-10-19 21:00:08 +0900

    Create base_page.py

---
+++ pages/base_page.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+
+class BasePage:
+def __init__(self, driver, base_url: str):
+self.driver = driver
+self.base_url = base_url.rstrip('/')
+
+
+def open(self, path: str = ''):
+url = self.base_url + (('/' + path.lstrip('/')) if path else '')
+self.driver.get(url)
+
+
+def find(self, locator):
+return self.driver.find_element(*locator)
+
+
+def finds(self, locator):
+return self.driver.find_elements(*locator)
+
+
+def wait_visible(self, locator, timeout=10):
+return WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+
+
+def wait_clickable(self, locator, timeout=10):
+return WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
